f67f193d909dbfb9f39b1bb266994fc70ef26930
RaquelRamos <raquel.ramos8@alu.uclm.es>
2017-10-06 11:11:08 +0200

    Actualización del menú

--- src/dominio/main.java
+++ src/dominio/main.java
@@ -18,23 +18,47 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // LECTURA POR TECLADO
-        int x = leer.entero("Introduzca la posicion en filas del tractor");
-        int y = leer.entero("Introduzca la posicion en columnas del tractor");
-        int k = leer.entero("Introduzca el número deseado de arena a distribuir de cada casilla");
-        int MAX = leer.entero("Introduzca el número máximo de arena en cada casilla: ");
-        int C = leer.entero("Ingrese el numero de filas del terreno: ");
-        int F = leer.entero("Ingrese el numero de columnas del terreno: ");
-        /**
-         * Crear objeto terreno con longitud dada y un maximo dado
-         */
-        Terreno t = new Terreno(C,F,MAX);
-        // Guardamos los datos en una lista
-        int arrayDatos[] ={x,y,k,MAX,C,F};
-        // Escribimos en el fichero con la lista y la matriz
-        EscribirFichero.introducirDatosFichero(arrayDatos,t.getCasillas());
+        boolean op=true;
+	int opcion=0;
+    
+	do{
+            System.out.println("\n\n+++++++ MENU PRINCIPAL +++++++");
+            System.out.print("1 ---> Introducir datos por tecaldo");
+            System.out.print("\n2 ---> Leer datos de un fichero");
+            System.out.print("\n3 ---> Salir");
+            System.out.print("\n");
+                opcion=leer.entero("\nElija una opcion:");
+            System.out.println("");
+
+            switch(opcion){ // LECTURA POR TECLADO
+                case 1:
+                    int x = leer.entero("Introduzca la posicion en filas del tractor");
+                    int y = leer.entero("Introduzca la posicion en columnas del tractor");
+                    int k = leer.entero("Introduzca el número deseado de arena a distribuir de cada casilla");
+                    int MAX = leer.entero("Introduzca el número máximo de arena en cada casilla: ");
+                    int C = leer.entero("Ingrese el numero de filas del terreno: ");
+                    int F = leer.entero("Ingrese el numero de columnas del terreno: ");
+                /**
+                 * Crear objeto terreno con longitud dada y un maximo dado
+                 */
+                Terreno t = new Terreno(C,F,MAX);
+                // Guardamos los datos en una lista
+                int arrayDatos[] ={x,y,k,MAX,C,F};
+                // Escribimos en el fichero con la lista y la matriz
+                EscribirFichero.introducirDatosFichero(arrayDatos,t.getCasillas());
+                break;    
+                
+                case 2: //LECTURA POR FICHERO
+                    
+                    break;
+                
+                case 3: //SALIR
+                op=false;
+                System.out.print("Gracias por sus consutlas");
+                break;
+            }
+        }while(op==true);
+    
+    
     }
-    
-    
-    
 }
